67ef902eefcc14ab76a082aed1b4ecaece4c5b4d
pluviolithic <e-mckanna@onu.edu>
2022-12-08 06:58:13 -0500

    begin adding voting and randomization

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import os
+import json
 import shutil
+import strawpoll
 import interactions
 from os.path import exists
 from dotenv import load_dotenv
@@ -16,6 +18,9 @@ guildID = int(str(os.getenv("guild-id")))
 # initialize empty settings file if none exists
 if not exists("./settings.json"):
     shutil.copyfile("default.json", "settings.json")
+with open("sessiondata.json", "w") as sessionDataFile:
+    json.dump({}, sessionDataFile)
+    
 
 bot = interactions.Client(token=str(discordToken))
 
@@ -43,5 +48,62 @@ async def configure(ctx, name, value):
     else:
         changeSetting(name, value)
         await ctx.send(f"{name} set to {value} successfully!")
+
+@bot.command(
+    name="categorypoll",
+    description="Create a strawpoll to select a category.",
+    scope=guildID,
+)
+async def categorypoll(ctx):
+    pass
+
+@bot.command(
+    name="restaurantpoll",
+    description="Create a strawpoll to select a restaurant.",
+    scope=guildID,
+    options=[
+        interactions.Option(
+            name="unrandomized",
+            description="Should results be based on previous poll? y/n",
+            type=interactions.OptionType.STRING,
+            required=False,
+        ),
+    ]
+)
+async def restaurantpoll(ctx, unrandomized="n"):
+    randomize = unrandomized.lower()[0] != "y"
+    pass
+
+@bot.command(
+    name="showresults",
+    description="Create a strawpoll to select restaurant.",
+    scope=guildID,
+    options=[
+        interactions.Option(
+            name="polltype",
+            description="categorypoll or restarantpoll?",
+            type=interactions.OptionType.STRING,
+            required=False,
+        ),
+    ]
+)
+async def showresults(ctx, polltype):
+    pass
+
+@bot.command(
+    name="pickrestaurant",
+    description="Randomly pick a nearby restaurant.",
+    scope=guildID,
+    options=[
+        interactions.Option(
+            name="category",
+            description="Optionally include category name or use last category poll result.",
+            type=interactions.OptionType.STRING,
+            required=False,
+        ),
+    ]
+)
+async def pickrestaurant(ctx, category):
+    pass
 
 bot.start()
